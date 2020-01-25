f218f76c689c2ba61186276e94d72ff9a501969c
Suyeon Kang <tndusrkd92@gmail.com>
2020-01-25 09:12:48 +0900

    I add a sticky function onthe  navbar using waypoints  and change the header's theme color from black to gold. And I create a partnership section in Join with us.html.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// WAYPOINTS
+// WAYPOINTS - FADE IN 
 $('.wp--home-1').waypoint(function (direction) {
     $('.wp--home-1').addClass('animated fadeIn');
 }, {
@@ -20,12 +20,60 @@ $(".person-1").hover(function () {
 });
 
 
-// OPEN AND CLOSE A POPUP
+// POPUP - OPEN AND CLOSE 
 $(".popup__trigger").click(function () {
     $(".popup").show();
 });
 
-
 $(".popup__close").click(function () {
     $(".popup").hide();
+});
+
+// POPUP - HIDE BY CLICKING
+var mouse_is_inside = false;
+
+$(document).ready(function () {
+    $('.popup__content').hover(function () {
+        mouse_is_inside = true;
+    }, function () {
+        mouse_is_inside = false;
+    });
+
+    $("body").mouseup(function () {
+        if (!mouse_is_inside) $('.popup').hide();
+    });
+});
+
+
+// TOGGLE NAVIGATION 
+$(".navigation__button").click(function () {
+    $(".navigation__nav").toggle();
+});
+
+// STICKY NAVIGATION - HOME
+$('.mantra').waypoint(function (direction) {
+    if (direction == "down") {
+        $('.navigation').addClass('sticky');
+        $(".navigation__logo").attr("src", "img/logo-black.png");
+
+    } else {
+        $('.navigation').removeClass('sticky');
+        $(".navigation__logo").attr("src", "img/logo-gold.png");
+    }
+}, {
+    offset: '30%;'
+});
+
+// STICKY NAVIGATION - SUBPAGES
+$('main').waypoint(function (direction) {
+    if (direction == "down") {
+        $('.navigation').addClass('sticky');
+        $(".navigation__logo").attr("src", "img/logo-black.png");
+
+    } else {
+        $('.navigation').removeClass('sticky');
+        $(".navigation__logo").attr("src", "img/logo-gold.png");
+    }
+}, {
+    offset: '30%;'
 });
