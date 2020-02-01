5ac8350195f8e4e4e14b07a4d389e090feca02d2
Suyeon Kang <tndusrkd92@gmail.com>
2020-02-01 21:12:42 +0900

    I retouch the images and include it into the interveiw and team section  except for three excutives. And I complete the two part of branch on join with us except for branch-inception.

--- js/index.js
+++ js/index.js
@@ -34,21 +34,58 @@ $('.wp--home-6').waypoint(function (direction) {
     $('.wp--home-6').addClass('animated fadeIn');
 }, {
     offset: '80%'
-});
+})
 
 // About 
-$('.wp--home-7').waypoint(function (direction) {
-    $('.wp--home-7').addClass('animated fadeIn');
+$('.wp--about-7').waypoint(function (direction) {
+    $('.wp--about-7').addClass('animated fadeIn');
+}, {
+    offset: '80%'
+});
+
+$('.wp--about-8').waypoint(function (direction) {
+    $('.wp--about-8').addClass('animated fadeIn');
+}, {
+    offset: '80%'
+});
+
+$('.wp--about-9').waypoint(function (direction) {
+    $('.wp--about-9').addClass('animated fadeIn');
 }, {
     offset: '80%'
 });
 
 /********  IMAGE ON HOVER *********/
-$(".person-1").hover(function () {
-    $(".person-1").attr("src", "img/sample-2.jpg");
+$(".person-2").hover(function () {
+    $(".person-2").attr("src", "img/sample-2.jpg");
 }, function () {
-    $(".person-1").attr("src", "img/sample-1.jpg");
+    $(".person-2").attr("src", "img/sample-1.jpg");
 });
+
+$(".person-4").hover(function () {
+    $(".person-4").attr("src", "img/Jeong Haemin-2.jpg"); 
+}, function () {
+    $(".person-4").attr("src", "img/Jeong Haemin-1.jpg");
+});
+
+$(".person-5").hover(function () {
+    $(".person-5").attr("src", "img/Lee Hana-2.jpg"); 
+}, function () {
+    $(".person-5").attr("src", "img/Lee Hana-1.jpg");
+});
+
+$(".person-6").hover(function () {
+    $(".person-6").attr("src", "img/Jung Hana-2.jpg"); 
+}, function () {
+    $(".person-6").attr("src", "img/Jung Hana-1.jpg");
+});
+
+$(".person-7").hover(function () {
+    $(".person-7").attr("src", "img/Lee Joonha-2.jpg"); 
+}, function () {
+    $(".person-7").attr("src", "img/Lee Joonha-1.jpg");
+});
+
 
 
 /******** POPUP *********/ 
