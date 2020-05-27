544a2cd1e8256d2f4d7259a76d78245e1b234d07
Suyeon Kang <tndusrkd92@gmail.com>
2020-05-27 20:42:54 +0900

    Add preview video and modal in index.html

--- js/index.js
+++ js/index.js
@@ -1,203 +1,167 @@
+const strings = {
+    modalCloseBtn: document.querySelector('.notice-btn'),
+    modal: document.querySelector('.notice'),
+    popup: '.popup',
+    popupTrigger: '.popup__trigger',
+    popupClose: '.popup__close',
+    popupContent: '.popup__content',
+    subHeaderBtn: '.arrow-button',
+    translateKo: '.sub-header__text-description--ko',
+    navBtn: '.navigation__button',
+    navbar: '.navigation__nav',
+    navigation: '.navigation',
+    navLogo: '.navigation__logo',
+    video: '.video'
+};
+
+/********  Modal *********/
+if (strings.modal) {
+    strings.modalCloseBtn.addEventListener('click', e => {
+        e.preventDefault();
+        strings.modal.parentNode.removeChild(strings.modal);
+    });
+};
+
 /******** WAYPOINTS *********/
 // Home
-$('.wp--home-1').waypoint(function (direction) {
-    $('.wp--home-1').addClass('animated tada');
-}, {
-    offset: '50%'
-});
+const offset = '80%';
+const fadeIn = 'animated fadeIn';
 
 $('.wp--home-2').waypoint(function (direction) {
-    $('.wp--home-2').addClass('animated fadeIn');
+    $('.wp--home-2').addClass(fadeIn);
 }, {
-    offset: '80%'
+    offset: offset
 });
 
 $('.wp--home-3').waypoint(function (direction) {
-    $('.wp--home-3').addClass('animated fadeIn');
+    $('.wp--home-3').addClass(fadeIn);
 }, {
-    offset: '80%'
+    offset: offset
 });
 
 // About 
 $('.wp--about-1').waypoint(function (direction) {
-    $('.wp--about-1').addClass('animated fadeIn');
+    $('.wp--about-1').addClass(fadeIn);
 }, {
-    offset: '80%'
+    offset: offset
 });
 
 $('.wp--about-2').waypoint(function (direction) {
-    $('.wp--about-2').addClass('animated fadeIn');
+    $('.wp--about-2').addClass(fadeIn);
 }, {
-    offset: '80%'
+    offset: offset
 });
 
 $('.wp--about-3').waypoint(function (direction) {
-    $('.wp--about-3').addClass('animated fadeIn');
+    $('.wp--about-3').addClass(fadeIn);
 }, {
-    offset: '80%'
+    offset: offset
 });
 
 $('.wp--about-4').waypoint(function (direction) {
-    $('.wp--about-4').addClass('animated fadeIn');
+    $('.wp--about-4').addClass(fadeIn);
 }, {
-    offset: '80%'
+    offset: offset
 });
 
 $('.wp--about-5').waypoint(function (direction) {
-    $('.wp--about-5').addClass('animated fadeIn');
+    $('.wp--about-5').addClass(fadeIn);
 }, {
-    offset: '80%'
+    offset: offset
 });
 
 // Join with us
 $('.wp--partnership-1').waypoint(function (direction) {
-    $('.wp--partnership-1').addClass('animated fadeIn');
+    $('.wp--partnership-1').addClass(fadeIn);
 }, {
-    offset: '80%'
+    offset: offset
 });
 
 $('.wp--partnership-2').waypoint(function (direction) {
-    $('.wp--partnership-2').addClass('animated fadeIn');
+    $('.wp--partnership-2').addClass(fadeIn);
 }, {
-    offset: '80%'
+    offset: offset
 });
 
 $('.wp--partnership-3').waypoint(function (direction) {
-    $('.wp--partnership-3').addClass('animated fadeIn');
+    $('.wp--partnership-3').addClass(fadeIn);
 }, {
-    offset: '80%'
+    offset: offset
 });
 
 $('.wp--partnership-4').waypoint(function (direction) {
-    $('.wp--partnership-4').addClass('animated fadeIn');
+    $('.wp--partnership-4').addClass(fadeIn);
 }, {
-    offset: '80%'
+    offset: offset
 });
-
-
-/********  IMAGE ON HOVER *********/
-$(".person-1").hover(function () {
-    $(".person-1").attr("src", "img/Park Cheonwoong-2.jpg");
-}, function () {
-    $(".person-1").attr("src", "img/Park Cheonwoong-1.jpg");
-});
-
-$(".person-2").hover(function () {
-    $(".person-2").attr("src", "img/Wang Hanna-2.jpg");
-}, function () {
-    $(".person-2").attr("src", "img/Wang Hanna-1.jpg");
-});
-
-$(".person-3").hover(function () {
-    $(".person-3").attr("src", "img/Park Kwansoo-2.jpg");
-}, function () {
-    $(".person-3").attr("src", "img/Park Kwansoo-1.jpg");
-});
-
-$(".person-4").hover(function () {
-    $(".person-4").attr("src", "img/Jeong Haemin-2.jpg");
-}, function () {
-    $(".person-4").attr("src", "img/Jeong Haemin-1.jpg");
-});
-
-$(".person-5").hover(function () {
-    $(".person-5").attr("src", "img/Lee Hana-2.jpg");
-}, function () {
-    $(".person-5").attr("src", "img/Lee Hana-1.jpg");
-});
-
-$(".person-6").hover(function () {
-    $(".person-6").attr("src", "img/Jung Hana-2.jpg");
-}, function () {
-    $(".person-6").attr("src", "img/Jung Hana-1.jpg");
-});
-
-$(".person-7").hover(function () {
-    $(".person-7").attr("src", "img/Lee Joonha-2.jpg");
-}, function () {
-    $(".person-7").attr("src", "img/Lee Joonha-1.jpg");
-});
-
-$(".person-8").hover(function () {
-    $(".person-8").attr("src", "img/Kang Suyeon-2.jpg");
-}, function () {
-    $(".person-8").attr("src", "img/Kang Suyeon-1.jpg");
-});
-
-$(".person-9").hover(function () {
-    $(".person-9").attr("src", "img/Kang Soojeong-2.jpg");
-}, function () {
-    $(".person-9").attr("src", "img/Kang Soojeong-1.jpg");
-});
-
-
 
 /******** POPUP *********/
 // open and close
-$(".popup__trigger").click(function () {
-    $(".popup").show();
+$(strings.popupTrigger).click(function () {
+    $(strings.popup).show();
 });
 
-$(".popup__close").click(function () {
-    $(".popup").hide();
+$(strings.popupClose).click(function () {
+    $(strings.popup).hide();
 });
 
-// hide by clicking
+// hide on click
 var mouse_is_inside = false;
 
-$('.popup__content').hover(function () {
+$(strings.popupContent).hover(function () {
     mouse_is_inside = true;
 }, function () {
     mouse_is_inside = false;
 });
 
 $("body").mouseup(function () {
-    if (!mouse_is_inside) $('.popup').hide();
+    if (!mouse_is_inside) $(strings.popup).hide();
 });
 
 
 /********  NAVIGATION *********/
 // toggle
-$(".navigation__button").click(function () {
-    $(".navigation__nav").toggle();
+$(strings.navBtn).click(function () {
+    $(strings.navbar).toggle();
 });
 
 // sticky - home
-$('.mantra').waypoint(function (direction) {
+const navOffset = '30%';
+
+$(strings.video).waypoint(function (direction) {
     if (direction == "down") {
-        $('.navigation').addClass('sticky');
-        $(".navigation__logo").attr("src", "img/logo-black.png");
+        $(strings.navigation).addClass('sticky');
+        $(strings.navLogo).attr("src", "img/logo-black.png");
 
     } else {
-        $('.navigation').removeClass('sticky');
-        $(".navigation__logo").attr("src", "img/logo-gold.png");
+        $(strings.navigation).removeClass('sticky');
+        $(strings.navLogo).attr("src", "img/logo-gold.png");
     }
 }, {
-    offset: '30%;'
+    offset: navOffset
 });
 
 // sticky - subpages
 $('main').waypoint(function (direction) {
     if (direction == "down") {
-        $('.navigation').addClass('sticky');
-        $(".navigation__logo").attr("src", "img/logo-black.png");
+        $(strings.navigation).addClass('sticky');
+        $(strings.navLogo).attr("src", "img/logo-black.png");
 
     } else {
-        $('.navigation').removeClass('sticky');
-        $(".navigation__logo").attr("src", "img/logo-gold.png");
+        $(strings.navigation).removeClass('sticky');
+        $(strings.navLogo).attr("src", "img/logo-gold.png");
     }
 }, {
-    offset: '30%;'
+    offset: navOffset
 });
 
-
 // translate with button on Subheader
-$(".arrow-button").click(function () {
-    $(".sub-header__text-description--ko").toggle();
+$(strings.subHeaderBtn).click(function () {
+    $(strings.translateKo).toggle();
 });
 
 /********  Kakao Map *********/
-// 봉선 행복점
+// 봉선 행복점  
 new daum.roughmap.Lander({
     "timestamp": "1583810604533",
     "key": "xfak",
